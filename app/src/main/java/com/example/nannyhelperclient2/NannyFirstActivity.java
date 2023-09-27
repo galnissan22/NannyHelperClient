8b4342378dc9a9decefca81a3b0eb959728aa125
Gal Nissan <gal.nissan22@gmail.com>
2023-09-27 12:17:41 +0300

    Add NannyFirstActivity back-button

--- app/src/main/java/com/example/nannyhelperclient2/NannyFirstActivity.java
+++ app/src/main/java/com/example/nannyhelperclient2/NannyFirstActivity.java
@@ -3,6 +3,8 @@ package com.example.nannyhelperclient2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class NannyFirstActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class NannyFirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nanny_first);
+
+        ImageButton back_button = findViewById(R.id.backbtn);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NannyFirstActivity.super.onBackPressed();
+            }
+        });
     }
+
 }
