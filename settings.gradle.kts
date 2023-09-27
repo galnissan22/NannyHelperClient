752bdd12f346d2d75ca9b2874ce1cb251996d5ec
Gal Nissan <gal.nissan22@gmail.com>
2023-09-27 10:41:06 +0300

    WIP - create initial app

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Nanny Helper Client2"
+include(":app")
+ 
