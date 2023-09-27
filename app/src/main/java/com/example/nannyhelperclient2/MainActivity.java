70b1321c04672090c26ac629da15f72c0fa4a92a
Gal Nissan <gal.nissan22@gmail.com>
2023-09-27 11:12:02 +0300

    make login button start NannyFirstActivity

--- app/src/main/java/com/example/nannyhelperclient2/MainActivity.java
+++ app/src/main/java/com/example/nannyhelperclient2/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.nannyhelperclient2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +15,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
+        MaterialButton login_button = findViewById(R.id.loginbtn);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), NannyFirstActivity.class));
+            }
+        });
     }
 }
